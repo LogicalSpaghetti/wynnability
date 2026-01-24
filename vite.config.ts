af9ca3ed28c5440bf3d8ef163749284d7fa073aa
Logical Spaghetti <130314555+LogicalSpaghetti@users.noreply.github.com>
2026-01-24 00:16:03 -0500

    Trying to fix build Pages failing without having to fix all TS errors.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import {defineConfig} from 'vite'
+import {defineConfig} from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: `/wynnability/`
-})
+    base: `/wynnability/`,
+});
